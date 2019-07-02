0d29d5f76e96eeea3e4378d84ce38b029e8e6879
EsdrasRafael <esdrasrocha1122@gmail.com>
2019-07-01 23:03:04 -0300

    Não existe Escolha de Nação.

---
+++ Battle-Board-Games/Controllers/IniciarBatalha.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static BattleBoardGame.Model.Factory.AbstractFactoryExercito;
+
+namespace BattleBoardGames.Controllers
+{
+    public class IniciarBatalha
+    {
+        public Nacao NacaoExercitoBranco { get; set; }
+        public Nacao NacaoExercitoPreto { get; set; }
+    }
+}
---
+++ Model-Battle-Board-Games/BatalhasException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelBattleBoardGames
+{
+    public class BatalhasExeception : Exception
+    {
+        public BatalhasExeception(string mensagem)
+            : base(mensagem) { }
+    }
+}
